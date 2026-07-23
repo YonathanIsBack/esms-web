8e9e05a236a94e0b296edd08aee041294bb4da81
Yonathan Pandapotan <yonabaho@gmail.com>
2026-07-23 20:37:01 +0700

    Add ternary in basename

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.BASE_PATH ? process.env.BASE_PATH : "/"}>
+    <BrowserRouter
+      basename={process.env.BASE_PATH ? process.env.BASE_PATH : "/"}
+    >
       <Provider>
         <App />
       </Provider>
